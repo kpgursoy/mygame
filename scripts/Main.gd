089d7b7a905d64392565ba80efb207ada08c6a09
kpgursoy <gursoy.kaanpiri@gmail.com>
2026-08-05 15:22:10 +0300

    arti blogu cikarildi

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -30,7 +30,6 @@ var shapes: Array = [
 	[Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)],
 	[Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)],
 	[Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 1), Vector2i(0, 2)],
-	[Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)],
 	[Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(0, 2)],
 ]
 
